c02b38dbf94ad7283f06530ef28c0825f6865aae
alireza <alireza.jvh98@gmail.com>
2022-08-10 12:26:42 +0430

    update

--- run-bash/multidataset_bash/HSML_multidataset_1shot.sh
+++ run-bash/multidataset_bash/HSML_multidataset_1shot.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-python main.py --datasource=multidataset --metatrain_iterations=100000 --meta_batch_size=4 --update_batch_size=1 --update_lr=0.01 --num_updates=5 --num_classes=5 --logdir=./log/1shot/test-3/noval/631 --num_filters=32 --max_pool=True --hidden_dim=128 --emb_loss_weight=0.01 --test_set=True
+python main.py --datasource=multidataset --metatrain_iterations=100000 --meta_batch_size=4 --update_batch_size=1 --update_lr=0.01 --num_updates=5 --num_classes=5 --logdir=./log/1shot/test-3/noval/421 --num_filters=32 --max_pool=True --hidden_dim=128 --emb_loss_weight=0.01 --test_set=True
 
 
 # python main.py --datasource=multidataset  --meta_batch_size=4 --update_batch_size=1 --update_lr=0.01 --num_updates=5 --num_classes=5 --logdir=./log/1shot/test-3/221 --num_filters=32 --max_pool=True --hidden_dim=128 --emb_loss_weight=0.01 --test_set=True --test_epoch=94000 --train=False --test_dataset=0
