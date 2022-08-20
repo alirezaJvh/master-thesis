d6464c9d8bca151854ef67dac12e58a819707f8c
alireza <alireza.jvh98@gmail.com>
2022-08-20 11:39:06 +0430

    run on miniimagenet

--- run-bash/imagenet-bash/HSML_imagenet_5shot.sh
+++ run-bash/imagenet-bash/HSML_imagenet_5shot.sh
@@ -1,1 +1,1 @@
-python main.py --datasource=miniimagenet --metatrain_iterations=100000 --meta_batch_size=4 --update_batch_size=5 --update_lr=0.01 --num_updates=5 --num_classes=5 --logdir=./log/imagenet/5shot/test-3/221 --num_filters=32 --max_pool=True --hidden_dim=128 --emb_loss_weight=0.01
+python main.py --datasource=miniimagenet --metatrain_iterations=100000 --meta_batch_size=4 --update_batch_size=5 --update_lr=0.01 --num_updates=5 --num_classes=5 --logdir=./log/imagenet/5shot/test-3/421 --num_filters=32 --max_pool=True --hidden_dim=128 --emb_loss_weight=0.01
